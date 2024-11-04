0a1281126d69d8c727f39f46d424d71d411de87f
VuFeg <ngduogtruongvu@gmail.com>
2024-11-05 01:09:48 +0700

    feat: add redux and update feature navbar and sidebar

--- client/src/app/(component)/Navbar/index.tsx
+++ client/src/app/(component)/Navbar/index.tsx
@@ -1,16 +1,33 @@
 "use client";
 
-import { Bell, Menu, Settings, Sun } from "lucide-react";
+import { Bell, Menu, Moon, Settings, Sun } from "lucide-react";
 import Link from "next/link";
+import { useAppDispatch, useAppSelector } from "../../redux";
+import { setIsDarkMode, setIsSidebarCollapsed } from "@/src/state";
 
 const Navbar = () => {
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  const toggleDarkMode = () => {
+    dispatch(setIsDarkMode(!isDarkMode));
+  };
+
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  };
+
   return (
     <div className="flex justify-between items-center w-full mb-7">
       {/* LEFT SIDE */}
       <div className="flex justify-between items-center gap-5">
         <button
           className="p-3 bg-gray-100 rounded-full hover:bg-blue-100"
-          onClick={() => {}}
+          onClick={toggleSidebar}
         >
           <Menu className="size-4" />
         </button>
@@ -32,9 +49,15 @@ const Navbar = () => {
       <div className="flex justify-between items-center gap-5">
         <div className="hidden md:flex justify-between items-center gap-5">
           <div>
-            <button onClick={() => {}}>
-              <Sun className="text-gray-500" size={24} />
-            </button>
+            {isDarkMode ? (
+              <button onClick={toggleDarkMode}>
+                <Sun className="text-gray-500" size={24} />
+              </button>
+            ) : (
+              <button onClick={toggleDarkMode}>
+                <Moon className="text-gray-500" size={24} />
+              </button>
+            )}
           </div>
           <div className="relative">
             <Bell className="cursor-pointer text-gray-500" size={24} />
--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,20 +1,48 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import Navbar from "@/src/app/(component)/Navbar";
 import Sidebar from "@/src/app/(component)/Sidebar";
+import StoreProvider, { useAppSelector } from "./redux";
 
-type Props = {
-  children: React.ReactNode;
-};
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
 
-const DashboardWrapper = ({ children }: Props) => {
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("light");
+    }
+  });
+
   return (
-    <div className="light flex bg-gray-50 text-gray-900 w-full min-h-screen">
+    <div
+      className={`${
+        isDarkMode ? "dark" : "light"
+      } flex bg-gray-50 text-gray-900 w-full min-h-screen`}
+    >
       <Sidebar />
-      <main className="flex flex-col w-full h-full bg-gray-50 px-9 py-7 md:pl-24">
+      <main
+        className={`flex flex-col w-full h-full bg-gray-50 px-9 py-7 pl-24 ${
+          isSidebarCollapsed ? "md:pl-24" : "md:pl-72"
+        }`}
+      >
         <Navbar />
         {children}
       </main>
     </div>
+  );
+};
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
   );
 };
 
